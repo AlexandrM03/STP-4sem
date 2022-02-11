6ceff85a481a278316a47fda2dc7b3adad88c44b
Alexander <alexandrmozolevskiy@gmail.com>
2022-02-11 17:44:59 +0300

    initial commit

---
+++ Lab№2/src/by/bestu/it/Mozolevskiy/Main.java
@@ -0,0 +1,10 @@
+package by.bestu.it.Mozolevskiy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+        Test obj = new Test();
+        System.out.println(obj.getValue());
+    }
+}
---
+++ Lab№2/src/by/bestu/it/Mozolevskiy/Test.java
@@ -0,0 +1,7 @@
+package by.bestu.it.Mozolevskiy;
+
+public class Test {
+    public String getValue() {
+        return "Hello from my first project";
+    }
+}
